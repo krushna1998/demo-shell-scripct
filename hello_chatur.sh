8151ef36fdc84392b837b3e2f91615661312fdc3
krushna1998 <krushna2026@gmail.com>
2025-02-09 04:40:46 +0000

    added from dev

---
+++ hello_chatur.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Chatur: Adarniya sabhapati mahodaya"
+echo "Chatur: Aj hum sab milke chamatkar Karenge"
+echo "Rancho:Ratta marega to aisa hi hoga "
+echo "chatur:tichuk tichuk"
+echo "raju: itna hi tujhe to ek folder bana ke dikhana"
+mkdir -p rancho
+echo "rancho: Ye le, bana diya ,ro mat ab"
+echo "farhan: virun ne bezzati kardi, bola farhanitrate"
+echo "farhan:abba nahi manenge" > farhanitrate.txt
+
